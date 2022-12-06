d9632e03ed639edb0d585fa61176e136ba4ee922
TheBoringEdward <querulator78@gmail.com>
2022-12-06 09:34:50 +0100

    Did something more

--- src/de/edward/Tree.java
+++ src/de/edward/Tree.java
@@ -1,4 +1,23 @@
 package de.edward;
 
 public class Tree {
+
+    private Node root;
+
+    Tree(){
+        root = new Node(null, null, null, "");
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public String convert(String s){
+        String plain = "";
+        return plain; //Huh?
+    }
+
+    public void print(){
+        root.print();
+    }
 }
